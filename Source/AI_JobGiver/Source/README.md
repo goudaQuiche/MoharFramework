8e51d5b6d20b2c8cc2dd426e7dc27c2db86ac492
egaouyer@gmail.com <egaouyer@gmail.com>
2021-01-19 22:30:42 +0100

    no msg rand hediff giver

---
+++ Source/AI_JobGiver/Source/README.md
@@ -0,0 +1,10 @@
+# LTF_CloneBay
+Mod to dead colonist reviving opportunity through cloning
+
+> # Clone Bay
+> 
+> ## Meat masher
+> 
+> ## Casket
+>
+> ## Scanner
---
+++ Source/MoharHediffs/NoMsgRandHediffGiver/NoMsgRandHediffGiver.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using System;
+
+namespace MoharHediffs
+{
+    public class NoMsgRandHediffGiver : HediffGiver
+    {
+        public float mtbDays;
+
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            if (Rand.MTBEventOccurs(mtbDays, 60000f, 60f))
+                TryApply(pawn);
+        }
+    }
+    
+
+}
