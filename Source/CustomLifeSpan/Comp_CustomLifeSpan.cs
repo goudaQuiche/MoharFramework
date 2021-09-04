373d0e4aeb4b076ae8cc7feebfea572a2b75b749
goudaQuiche <egaouyer@gmail.com>
2021-09-04 12:03:59 +0200

    Renamed LifeSpan into CustomLifeSpan; Added Thing support for CLS;

--- Source/CustomLifeSpan/Comp_CustomLifeSpan.cs
+++ Source/CustomLifeSpan/Comp_CustomLifeSpan.cs
@@ -12,32 +12,34 @@ namespace CustomLifeSpan
 		public int age = -1;
 
         public CompProperties_Lifespan Props => (CompProperties_Lifespan)props;
-        bool myDebug => Props.debug;
-        Pawn pawn;
-        Building building;
+        bool MyDebug => Props.debug;
+        Pawn pawn = null;
+        Building building = null;
+        Thing thing = null;
 
-        bool IsPawn;
-        bool IsBuilding;
+        bool IsPawn => pawn != null;
+        bool IsBuilding => building != null;
+        bool IsThing => thing != null;
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             /*
             if (!parent.IsPawn())
             {
-                Tools.Warn("this is a comp is meant for a pawn, this will fail", myDebug);
+                if(MyDebug) Log.Warning("this is a comp is meant for a pawn, this will fail");
             }
             */
             if (parent is Pawn nPawn)
             {
                 pawn = nPawn;
-                IsPawn = true;
-                IsBuilding = false;
             }
             else if (parent is Building nBuilding)
             {
                 building = nBuilding;
-                IsPawn = false;
-                IsBuilding = true;
+            }
+            else
+            {
+                thing = parent;
             }
         }
 
@@ -55,6 +57,33 @@ namespace CustomLifeSpan
             }
         }
 
+        private int GetMeatAmount()
+        {
+            if (IsPawn && pawn.def.GetStatValueAbstract(StatDefOf.MeatAmount) <= 0)
+            {
+                if(MyDebug) Log.Warning("pawn but no stat meatamount (is <= 0)");
+                return 0;
+            }
+
+            //if( IsBuilding && building.MaxHitPoints <= 0)
+            if (Props.relativeMeatAmount)
+            {
+                if ((IsBuilding || IsThing) && !parent.def.useHitPoints)
+                {
+                    if (MyDebug) Log.Warning("building/thing but relativeMeatAmount && no useHitpoints");
+                    return 0;
+                }
+
+                //meatAmount = GenMath.RoundRandom(pawn.GetStatValue(StatDefOf.MeatAmount));
+                if (IsPawn)
+                    return (int)pawn.GetStatValue(StatDefOf.MeatAmount);
+                else if (IsBuilding || IsThing)
+                    return (int)((float)parent.HitPoints / parent.MaxHitPoints);
+            }
+
+            return Props.fixedMeatAmount;
+        }
+
         private bool SpawnMeat()
         {
             if (!Props.spawnMeat)
@@ -62,89 +91,65 @@ namespace CustomLifeSpan
 
             if (!IsPawn && Props.meatToSpawn == null)
             {
-                Tools.Warn("parent is not pawn but tried to spawn meat while there is no Props.meatToSpawn, use spawnThing instead, maybe", myDebug);
+                if (MyDebug) Log.Warning("parent is not pawn but tried to spawn meat while there is no Props.meatToSpawn, use spawnThing instead, maybe");
                 return false;
             }
 
             if (Props.meatToSpawn == null && pawn.RaceProps.meatDef == null)
             {
-                Tools.Warn("Found no meat def for pawn or stat of meatamount is <= 0", myDebug);
-                return false;
-            }
-                
-            if ( IsPawn && pawn.def.GetStatValueAbstract(StatDefOf.MeatAmount) <= 0)
-            {
-                Tools.Warn("pawn but no stat meatamount (is <= 0)", myDebug);
+                if (MyDebug) Log.Warning("Found no meat def for pawn or stat of meatamount is <= 0");
                 return false;
             }
 
-             if( IsBuilding && building.MaxHitPoints <= 0)
-            {
-                Tools.Warn("building but no stat maxhitpoints (is <= 0)", myDebug);
-                return false;
-            }
-
-            int meatAmount = 0;
-            if (Props.relativeMeatAmount)
-            {
-                //meatAmount = GenMath.RoundRandom(pawn.GetStatValue(StatDefOf.MeatAmount));
-                if(IsPawn)
-                    meatAmount = (int)pawn.GetStatValue(StatDefOf.MeatAmount);
-                else if(IsBuilding)
-                    meatAmount = (int)((float)building.HitPoints/ building.MaxHitPoints);
-            }
-            else
-            {
-                meatAmount = Props.fixedMeatAmount;
-            }
+            int meatAmount = GetMeatAmount();
 
             if (meatAmount <= 0)
             {
-                Tools.Warn("Calculated meat amount is <= 0, nothing to do", myDebug);
+                if (MyDebug) Log.Warning("Calculated meat amount is <= 0, nothing to do");
                 return false;
             }
 
             ThingDef thingToSpawn = null;
             if (IsPawn)
                 thingToSpawn = (Props.meatToSpawn == null) ? pawn.RaceProps.meatDef : Props.meatToSpawn;
-            else if (IsBuilding)
+            else if (IsBuilding || IsThing)
                 thingToSpawn = Props.meatToSpawn;
 
             if (thingToSpawn == null)
             {
-                Tools.Warn("ThingToSpawn is null, cant work", myDebug);
+                if (MyDebug) Log.Warning("ThingToSpawn is null, cant work");
                 return false;
             }
 
             int pileNum = Props.meatPilesIntRange.RandomInRange;
-            Tools.Warn("Trying to spawn " + pileNum + " piles of " + pawn.RaceProps.meatDef + "(x" + meatAmount + ")", myDebug);
+            if (MyDebug) Log.Warning("Trying to spawn " + pileNum + " piles of " + thingToSpawn + "(x" + meatAmount + ")");
             int loopBreaker = 20;
             for (int i = 0; i < pileNum; i++)
             {
-                Tools.Warn(i + "/" + pileNum + " meat left: "+meatAmount, myDebug);
+                if (MyDebug) Log.Warning(i + "/" + pileNum + " meat left: " + meatAmount);
                 if (loopBreaker-- < 0)
                 {
-                    Tools.Warn("Had to break the meat loop, bc it would not end", myDebug);
+                    if (MyDebug) Log.Warning("Had to break the meat loop, bc it would not end");
                     return false;
                 }
                     
                 int currentPileAmount = Rand.Range(1, meatAmount);
                 meatAmount -= currentPileAmount;
 
-                Tools.Warn(i + "/" + pileNum + " spawning: " + currentPileAmount, myDebug);
+                if(MyDebug) Log.Warning(i + "/" + pileNum + " spawning: " + currentPileAmount);
                 bool Didit = false;
 
                 Didit = Lifespan_Utility.TryDoSpawn(parent, thingToSpawn, currentPileAmount, Props.spawnMaxAdjacent, Props.tryToUnstack, Props.inheritFaction, Props.spawnForbidden, Props.showMessageIfOwned);
 
                 if (!Didit)
                 {
-                    Tools.Warn("Had to break the meat loop, bc it cant spawn things", myDebug);
+                    if (MyDebug) Log.Warning("Had to break the meat loop, bc it cant spawn things");
                     return false;
                 }
 
                 if (meatAmount <= 0)
                 {
-                    Tools.Warn("Had to break the meat loop, bc it did its job", myDebug);
+                    if (MyDebug) Log.Warning("Had to break the meat loop, bc it did its job");
                     break;
                 }
                     
@@ -152,51 +157,55 @@ namespace CustomLifeSpan
 
             return true;
         }
-
-        private bool SpawnThing()
+        private int GeThingAmount()
         {
-            if (!Props.spawnThing)
-                return false;
-
-            int thingAmount = Props.thingNumIfFullBody;
+            int tmpValue = 0;
+            if (Props.thingNumIfFullBody >= 0)
+                tmpValue = Props.thingNumIfFullBody;
 
             if (Props.relativeThingAmount)
             {
                 if (IsPawn && pawn.def.GetStatValueAbstract(StatDefOf.MeatAmount) <= 0)
                 {
-                    Tools.Warn("parent is pawn but no meat amount in def while relativeThingAmount is set, cant spawn things", myDebug);
-                    return false;
+                    if (MyDebug) Log.Warning("parent is pawn but no meat amount in def while relativeThingAmount is set, cant spawn things");
+                    return 0;
                 }
-                if (IsBuilding && building.MaxHitPoints <= 0)
+                if ((IsBuilding || IsThing) && !parent.def.useHitPoints)
                 {
-                    Tools.Warn("parent is building but no HP set in def while relativeThingAmount is set, cant spawn things", myDebug);
-                    return false;
+                    if (MyDebug) Log.Warning("parent is building but no HP set in def while relativeThingAmount is set, cant spawn things");
+                    return 0;
                 }
 
                 if (IsPawn)
-                    thingAmount *=
-                    GenMath.RoundRandom(pawn.GetStatValue(StatDefOf.MeatAmount)) / Mathf.RoundToInt(pawn.def.GetStatValueAbstract(StatDefOf.MeatAmount));
-                else if (IsBuilding)
-                    thingAmount = (int)(thingAmount * (float)building.HitPoints / building.MaxHitPoints);
+                    return tmpValue * GenMath.RoundRandom(pawn.GetStatValue(StatDefOf.MeatAmount)) / Mathf.RoundToInt(pawn.def.GetStatValueAbstract(StatDefOf.MeatAmount));
+                else if (IsBuilding || IsThing)
+                    return (int)(tmpValue * (float)parent.HitPoints / parent.MaxHitPoints);
             }
+            return 0;
+        }
+        private bool SpawnThing()
+        {
+            if (!Props.spawnThing)
+                return false;
 
+            int thingAmount = GeThingAmount();
 
             if (thingAmount <= 0)
             {
-                Tools.Warn("calculated thingAmount was <= 0, cant spawn things", myDebug);
+                if (MyDebug) Log.Warning("calculated thingAmount was <= 0, cant spawn things");
                 return false;
             }
 
 
             int pileNum = Props.thingPilesIntRange.RandomInRange;
-            Tools.Warn("Trying to spawn " + pileNum + " piles of " + Props.thingToSpawn + "(x"+thingAmount+")", myDebug);
+            if (MyDebug) Log.Warning("Trying to spawn " + pileNum + " piles of " + Props.thingToSpawn + "(x" + thingAmount + ")");
             int loopBreaker = 20;
             for (int i = 0; i < pileNum; i++)
             {
-                Tools.Warn(i + "/" + pileNum + " meat left: " + thingAmount, myDebug);
+                if(MyDebug) Log.Warning(i + "/" + pileNum + " meat left: " + thingAmount);
                 if (loopBreaker-- < 0)
                 {
-                    Tools.Warn("Had to break the thing loop, bc it would not end", myDebug);
+                    if(MyDebug) Log.Warning("Had to break the thing loop, bc it would not end");
                     return false;
                 }
                 int currentPileAmount = Rand.Range(1, thingAmount);
@@ -206,13 +215,13 @@ namespace CustomLifeSpan
 
                 if (!DidIt)
                 {
-                    Tools.Warn("Had to break the thing loop, bc it cant spawn things", myDebug);
+                    if(MyDebug) Log.Warning("Had to break the thing loop, bc it cant spawn things");
                     return false;
                 }
 
                 if (thingAmount <= 0)
                 {
-                    Tools.Warn("Had to break the thing loop, bc it did its job", myDebug);
+                    if(MyDebug) Log.Warning("Had to break the thing loop, bc it did its job");
                     break;
                 }
                     
@@ -225,21 +234,21 @@ namespace CustomLifeSpan
         {
             if (Props.filthDef == null)
             {
-                Tools.Warn("no Props.filthDef, cant SpawnFilth", myDebug);
+                if(MyDebug) Log.Warning("no Props.filthDef, cant SpawnFilth");
                 return false;
             }
 
             Thing refThing = Lifespan_Utility.ThingInCaseOfDeath(parent);
             if (refThing == null)
             {
-                Tools.Warn("no refThing in caase of death, cant SpawnFilth", myDebug);
+                if(MyDebug) Log.Warning("no refThing in caase of death, cant SpawnFilth");
                 return false;
             }
 
             int filthNum = Props.filthIntRange.RandomInRange;
             if(filthNum <= 0)
             {
-                Tools.Warn("filthNum <= 0, cant SpawnFilth", myDebug);
+                if(MyDebug) Log.Warning("filthNum <= 0, cant SpawnFilth");
                 return false;
             }
             for (int i = 0; i < filthNum; i++)
@@ -255,21 +264,21 @@ namespace CustomLifeSpan
         {
             if (!Props.spawnMote)
             {
-                Tools.Warn("no Props.spawnMote, wont SpawnMote", myDebug);
+                if(MyDebug) Log.Warning("no Props.spawnMote, wont SpawnMote");
                 return false;
             }
 
             Thing refThing = Lifespan_Utility.ThingInCaseOfDeath(parent);
             if (refThing == null)
             {
-                Tools.Warn("no refThing in caase of death, cant SpawnMote", myDebug);
+                if(MyDebug) Log.Warning("no refThing in case of death, cant SpawnMote");
                 return false;
             }
 
             int moteNum = Props.moteNumRange.RandomInRange;
             if (moteNum <= 0)
             {
-                Tools.Warn("moteNum <= 0, cant SpawnMote", myDebug);
+                if(MyDebug) Log.Warning("moteNum <= 0, cant SpawnMote");
                 return false;
             }
 
@@ -280,7 +289,7 @@ namespace CustomLifeSpan
                 origin.y += Rand.Range(0, Props.moteRadius) * (Rand.Chance(.5f) ? -1 : 1);
                 if (Props.moteDef == null)
                 {
-                    Tools.Warn("no Props.moteDef, will spawn smoke", myDebug);
+                    if(MyDebug) Log.Warning("no Props.moteDef, will spawn smoke");
                     FleckMaker.ThrowSmoke(origin, refThing.Map, Props.moteScale.RandomInRange);
                     //Lifespan_Utility.ThrowCustomSmoke(ThingDefOf.Mote_Smoke, origin, refThing.Map, Props.moteScale.RandomInRange);
                 }
@@ -292,15 +301,15 @@ namespace CustomLifeSpan
             return true;
         }
 
-        private bool BuildingSaysGoodBye()
+        private bool ThingSaysGoodBye()
         {
-            if (!IsBuilding)
+            if (!IsBuilding && !IsThing)
             {
-                Tools.Warn("Non building tried to say good bye as building, wont destroy", myDebug);
+                if(MyDebug) Log.Warning("Tried to say good bye as building or thing, wont destroy");
                 return false;
             }
 
-            building.Destroy();
+            parent.Destroy();
             return true;
         }
 
@@ -308,17 +317,17 @@ namespace CustomLifeSpan
         {
             if (!IsPawn)
             {
-                Tools.Warn("Non pawn tried to say good bye as pawn, wont destroy", myDebug);
+                if(MyDebug) Log.Warning("Non pawn tried to say good bye as pawn, wont destroy");
                 return false;
             }
                 
-            Lifespan_Utility.RemoveBadMemoriesOfDeadPawn(pawn, myDebug);
+            Lifespan_Utility.RemoveBadMemoriesOfDeadPawn(pawn);
 
             if (pawn.Dead)
             {
                 if (pawn.Corpse == null)
                 {
-                    Tools.Warn("found no corpse to work with, wont do anything", myDebug);
+                    if(MyDebug) Log.Warning("found no corpse to work with, wont do anything");
                     return false;
                 }
                     
@@ -339,39 +348,39 @@ namespace CustomLifeSpan
         private void DestroyProcedure()
         {
             bool didMeat, didThing, didFilth, didMote, didDestroy;
-            didMeat= didThing= didFilth= didMote = didDestroy=false;
+            didMeat = didThing = didFilth = didMote = didDestroy = false;
 
             didMeat = SpawnMeat();
             didThing = SpawnThing();
             didFilth = SpawnFilth();
             didMote = SpawnMote();
             //SpawnCorpse();
-            Tools.Warn(parent.Label + " will try to autodestroy", myDebug);
+            if(MyDebug) Log.Warning(parent.Label + " will try to autodestroy");
 
             if (IsPawn)
-                didDestroy=PawnSaysGoodBye();
-            else if (IsBuilding)
-                didDestroy=BuildingSaysGoodBye();
+                didDestroy = PawnSaysGoodBye();
+            else if (IsBuilding || IsThing)
+                didDestroy = ThingSaysGoodBye();
 
-            Tools.Warn("didMeat="+didMeat+"; didThing="+didThing+"; didFilth="+didFilth+"; didMote="+didMote+";didDestroy="+didDestroy, myDebug);
+            if(MyDebug) Log.Warning("didMeat="+didMeat+"; didThing="+didThing+"; didFilth="+didFilth+"; didMote="+didMote+";didDestroy="+didDestroy);
             
         }
         public void MyTick(int tickNum)
         {
             if(parent != null && IsPawn && pawn.Dead)
             {
-                Tools.Warn("found dead pawn, starting DestroyProcedure()", myDebug);
+                if(MyDebug) Log.Warning("found dead pawn, starting DestroyProcedure()");
                 DestroyProcedure();
                 return;
             }
 
             if (IsPawn && pawn.Negligeable())
             {
-                Tools.Warn("found negligeable pawn, wont tick", myDebug);
+                if(MyDebug) Log.Warning("found negligeable pawn, wont tick");
                 return;
-            }else if (IsBuilding && building.Negligeable())
+            }else if ((IsBuilding || IsThing) && building.Negligeable())
             {
-                Tools.Warn("found negligeable building, wont tick", myDebug);
+                if(MyDebug) Log.Warning("found negligeable thing/building, wont tick");
                 return;
             }
                 
