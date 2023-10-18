4f9462a2ca522d5a9dfeff63f3bae04f86de1305
goudaQuiche <egaouyer@gmail.com>
2023-10-18 10:12:35 +0200

    Selecting all definitions that need to be added to meditate

---
+++ Source/CompSpawnSubplantUnlocker/HarmonyMain.cs
@@ -0,0 +1,48 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+
+namespace CSSU
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatchAll
+    {
+        static HarmonyPatchAll()
+        {
+            if (!MyDefs.IsRoyaltyLoaded)
+            {
+                Log.Warning("Mohar CSSU - No royalty, nothing to be done");
+                return;
+            }
+            
+            if(!MyDefs.HasDefsWithCSS)
+            {
+                Log.Warning("Mohar CSSU - Found no definition with CompSpawnSubplant, nothing to be done");
+                return;
+            }
+
+            //Harmony.DEBUG = true;
+            Harmony CSSU_HarmonyPatch = new Harmony("MoharFW.CompSpawnSubplantUnlocker");
+
+                
+            foreach(ThingDef td in MyDefs.AllDefsWithCSS)
+            {
+                Log.Warning("Found " + td.defName);
+            }
+
+            /*
+            if (MyDefs.HasFleshTypeWound)
+            {
+                if (Harmony_FleshTypeDef.Try_FleshTypeDef_ResolveWound_Patch(MoharBlood_HarmonyPatch))
+                    Log.Message(MoharBlood_HarmonyPatch.Id + " patched FleshTypeDef.ChooseWoundOverlay successfully.");
+            }
+            else if(MyDefs.HasDebug) Log.Warning("No fleshTypeWound found");
+            */
+        }
+    }
+}
---
+++ Source/CompSpawnSubplantUnlocker/MyDefs.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace CSSU
+{
+    public static class MyDefs
+    {
+        //CompProperties_SpawnSubplant
+        public static IEnumerable<ThingDef> AllDefsWithCSS = DefDatabase<ThingDef>.AllDefs.Where(
+            x =>
+            //x.defName != "Plant_TreeAnima" &&
+            x.modContentPack != null &&
+            x.modContentPack.PackageId != ModContentPack.CoreModPackageId &&
+            x.modContentPack.PackageId != ModContentPack.RoyaltyModPackageId &&
+            x.modContentPack.PackageId != ModContentPack.IdeologyModPackageId &&
+            x.modContentPack.PackageId != ModContentPack.BiotechModPackageId &&
+            !x.comps.NullOrEmpty() && 
+            x.comps.Any(y => y is CompProperties_SpawnSubplant)
+        );
+
+        public static bool HasDefsWithCSS => !AllDefsWithCSS.EnumerableNullOrEmpty();
+        public static bool IsRoyaltyLoaded => ModLister.CheckRoyalty("Mohar CSSU");
+
+    }
+}
